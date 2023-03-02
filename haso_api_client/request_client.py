06755be2b18eb3b28e6be8f09f1f8f4477325c93
HASEEB <75139477+Haseb-ali@users.noreply.github.com>
2023-03-02 21:52:02 +0500

    update request code

--- haso_api_client/request_client.py
+++ haso_api_client/request_client.py
@@ -1,16 +1,19 @@
-import requests
 import jwt
+import aiohttp
+from aiohttp import ClientSession, ClientResponseError
 from requests.auth import HTTPDigestAuth
 from requests_oauthlib import OAuth1
 from . import response_client
 
 
-class Request:
-    session = requests.Session()
-    adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+class AuthMethod:
+    BASIC = "Basic_Authentication"
+    DIGEST = "Digest_Authentication"
+    OAUTH = "OAuth_Authentication"
+    JWT = "jwt"
 
+
+class Request:
     def __init__(self, method, url, headers=None, params=None, data=None, json=None, auth=None):
         self.method = method
         self.url = url
@@ -21,51 +24,45 @@ class Request:
         self.auth = auth
 
     async def send(self):
-        try:
+        async with ClientSession() as session:
             if self.auth:
-                method_for_auth = self.auth.method_for_auth
-                if method_for_auth == "Basic_Authentication":
-                    self.auth = (self.auth.username, self.auth.password)
-                elif method_for_auth == "Digest_Authentication":
-                    self.auth = HTTPDigestAuth(self.auth.username, self.auth.password)
-                elif method_for_auth == "OAuth_Authentication":
-                    self.auth = OAuth1(
+                if self.auth.method_for_auth == AuthMethod.BASIC:
+                    session.auth = aiohttp.BasicAuth(
+                        self.auth.username, self.auth.password
+                    )
+                elif self.auth.method_for_auth == AuthMethod.DIGEST:
+                    session.auth = aiohttp.DigestAuth(
+                        self.auth.username, self.auth.password
+                    )
+                elif self.auth.method_for_auth == AuthMethod.OAUTH:
+                    session.auth = aiohttp.OAuth1Auth(
                         self.auth.client_key,
                         self.auth.client_secret,
                         self.auth.resource_owner_key,
                         self.auth.resource_owner_secret,
                     )
-
-                elif method_for_auth == "jwt":
+                elif self.auth.method_for_auth == AuthMethod.JWT:
                     token = jwt.encode(
                         {"user": "my_username"}, self.auth.secret_key, algorithm="HS256"
                     )
                     self.headers = {"Authorization": f"Bearer {token.decode()}"}
-                    self.session.auth = self.auth
-                    self.session.headers.update(self.headers)
-                    response = await self.session.request(
-                        method=self.method,
-                        url=self.url,
-                        headers=self.headers,
-                        params=self.params,
-                        json=self.json,
+
+            async with session.request(
+                method=self.method,
+                url=self.url,
+                headers=self.headers,
+                params=self.params,
+                json=self.json,
+                auth=session.auth,
+            ) as response:
+                if response.status >= 400:
+                    raise ClientResponseError(
+                        response.request_info,
+                        response.history,
+                        status=response.status,
+                        message=response.reason,
+                        headers=response.headers,
                     )
 
-            else:
-                self.session.auth = self.auth
-                self.session.headers.update(self.headers)
-                response = await self.session.request(
-                    method=self.method,
-                    url=self.url,
-                    auth=self.auth,
-                    headers=self.headers,
-                    params=self.params,
-                    json=self.json,
-                )
-
                 response_client.Response(response)
-                response.raise_for_status()
                 return response
-
-        except requests.exceptions.HTTPError as e:
-            raise e
